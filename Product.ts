029a76c16246e7fc8984053f974f2a7fbacc2785
Le-Nhu-Y <lenhuy20021989@gmail.com>
2022-12-08 11:44:04 +0700

    Add main.ts

---
+++ Product.ts
@@ -0,0 +1,21 @@
+export class Product{
+    private name: string
+    private price: number
+    constructor(name:string,
+                price:number) {
+        this.name = name;
+        this.price = price;
+    }
+    getName():string{
+        return this.name;
+    }
+    setName(name:string){
+       this.name = name;
+    }
+    getPrice():number{
+        return this.price;
+    }
+    setPrice(price:number){
+        this.price = price;
+    }
+}
